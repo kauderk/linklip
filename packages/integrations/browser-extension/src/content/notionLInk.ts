eb0641f165951847a8b466e10fe500acf0370893
kauderk <estudio1kau@gmail.com>
2023-12-01 00:16:57 -0500

    wip: streamline follower setup and cache

--- packages/integrations/browser-extension/src/content/notionLInk.ts
+++ packages/integrations/browser-extension/src/content/notionLInk.ts
@@ -1,19 +1,24 @@
 import { YTGIF_Config } from '@packages/yt-gif/src/core/lib/types/config'
 import { mutationTargets } from '@packages/yt-gif/src/core/init/observer/formatter/filter'
 import { createObserverAndDeployOnIntersection } from '@packages/yt-gif/src/core/init/observer/system/system'
-import Linklip, { createConfig } from '@packages/sandbox/src/app/App.svelte'
+//@ts-ignore
+import Linklip from '@packages/sandbox/src/app/App.svelte'
+import { createConfig } from '@packages/sandbox/src/app/integrations/followerCycles'
 import { player, storyboard } from '@packages/sandbox/src/app/timeline/context'
-import { stages } from '@packages/sandbox/src/app/follower/store'
+import { stages as createStagesCtx } from '@packages/sandbox/src/app/follower/store'
+import { follower as createFollower } from '@packages/sandbox/src/app/controller/follower'
+import { createSelectorsConfig } from '@packages/sandbox/src/app/integrations/selectorsConfig'
+import { cleanSubscribers } from '@packages/sandbox/src/lib/stores'
 
 export async function ObserveLinks_DeployLinklip() {
   const urlToSvelteMap = new Map<
     string,
     {
-      Linklip: Linklip
-      config: ReturnType<typeof createConfig>
+      update: (params: { notionHref: HTMLElement; containerID: string }) => void
+      cleanUp: (containerID: string) => () => void
     }
   >()
-  const stageCtx = ['Stages', stages()]
+  const stagesCtx = ['Stages', createStagesCtx()]
 
   return createObserverAndDeployOnIntersection(
     ['a.notion-link-token.notion-focusable-token.notion-enable-hover'],
@@ -46,108 +51,93 @@ export async function ObserveLinks_DeployLinklip() {
 
         const key = notionHref.href + containerID
 
-        function styleHost() {
-          // FIXME: this function is called too often
-          // by the Followers's branch function and the stage signal
-
-          const config = urlToSvelteMap.get(key)!.config
-          // FIXME: find a new way to abstract this, the styles are changing all over the place
-          if (config.stage.peek().mode == 'host' && isRendered(notionHref)) {
-            const maxWidth = notionHref.closest('.notranslate')?.clientWidth || 350
-            const width = config.rect.peek().width
-            const rect = {
-              width,
-              maxWidth,
-              height: width / config.aspectRatio.value,
-              maxHeight: maxWidth / config.aspectRatio.value,
-            }
-            createNotionHrefStyle(containerID, rect)
-          } else {
-            removeNotionHrefStyle(containerID)
-          }
-        }
         if (!urlToSvelteMap.has(key)) {
-          const context = new Map([
-            ['Player', player()],
-            ['Storyboard', storyboard()],
-            stageCtx,
-          ] as any)
+          const baseConfig = createConfig()
 
-          const config = createConfig()
+          const followerConfig = createSelectorsConfig({
+            aspectRatio: baseConfig.aspectRatio,
+            rect: baseConfig.rect,
+            stage: baseConfig.stage,
+            resizeMode: baseConfig.resizeMode,
+            notionRef: () => ({
+              notionHref,
+              containerID,
+            }),
+          })
+          const follower = createFollower({
+            rect: baseConfig.rect,
+            stage: baseConfig.stage,
+            ...followerConfig,
+          })
+
+          const stages = stagesCtx[1] as any // the signal types or generics don't work across packages
+          const unSharedFollower = stages.sharedControls.subscribe((stage: any) => {
+            const bottom = ['notionPage', 'notionMainScroller']
+            const top = ['notionTopBar']
+            const self = baseConfig.stage.peek()
+            if (stage.mode != 'host' || self.mode == 'free') return
+
+            // FIXME: no side effects
+            followerConfig.offset((_, original) => ({
+              y: !bottom.includes(stage.selector!) ? 10 : original.y,
+            }))
+            if ([...bottom, ...top].includes(self.selector!)) {
+              follower.trySwitchHost(stage.selector)
+            }
+          })
 
           const app = new Linklip({
             target: document.body,
             props: {
-              host: notionHref,
-              baseConfig: config,
-              styleHost,
+              config: baseConfig,
+              follower,
+              mount() {
+                // >.<
+                return cleanSubscribers(follower.mount(notionHref), unSharedFollower)
+              },
             },
-            context,
+            context: new Map([
+              ['Player', player()],
+              ['Storyboard', storyboard()],
+              ['Follower', follower],
+              stagesCtx,
+            ] as any),
           })
 
           urlToSvelteMap.set(key, {
-            Linklip: app,
-            config,
+            update({ notionHref, containerID }) {
+              // FIXME: requires cyclic update
+              followerConfig.notionRef = () => ({
+                notionHref,
+                containerID,
+              })
+              follower.changeHost(notionHref)
+            },
+            cleanUp(containerID) {
+              // FIXME: handle other selectors
+              const unStage = baseConfig.stage.subscribe(
+                followerConfig.selectors.notionLink.styleHost
+              )
+
+              return function onRemoved() {
+                unStage()
+                followerConfig.notionCleanUp(containerID)
+                console.log('destroying')
+              }
+            },
           })
+
+          // go to cleanUp
         } else {
-          // update
-          if (isRendered(notionHref)) {
-            urlToSvelteMap.get(key)!.Linklip.$set({ host: notionHref, styleHost })
+          if (document.contains(notionHref)) {
+            urlToSvelteMap.get(key)!.update({ notionHref, containerID })
           } else {
             console.warn('host is not in document')
             return
           }
         }
-
-        // FIXME: run before deploy, so the FollowerPortal can be created acurately
-        const unStage = urlToSvelteMap.get(key)!.config.stage.subscribe(styleHost)
-
-        return function onRemoved() {
-          unStage()
-          if (!document.querySelector(`.notion-text-block[data-block-id="${containerID}"]`)) {
-            // notion gave the block a new ID, so we need to destroy the old style
-            removeNotionHrefStyle(containerID)
-          }
-          console.log('destroying')
-        }
+        return urlToSvelteMap.get(key)!.cleanUp(containerID)
       },
     }
   )
-}
-function createNotionHrefStyle(containerID: string, _rect: any) {
-  const style = getNotionHrefStyle(containerID)
-
-  if (_rect.height < 50 || _rect.width < 50) {
-    console.log('Abort host styling', _rect)
-    return
-  }
-  // style the target to accommodate the portal
-  style.innerHTML = `
-		[data-block-id="${containerID}"] 
-		a.notion-link-token.notion-focusable-token.notion-enable-hover {
-			all: unset;
-
-			height: ${_rect.height}px; 
-			width: ${_rect.width}px;
-			max-width: ${_rect.maxWidth}px;
-			max-height: ${_rect.maxHeight}px;
-
-			outline: 1px solid black;
-			border-radius: .2em;
-
-			display: block;
-		}
-	`.trim()
-  document.head.appendChild(style)
-  style.id = styleID + containerID
-}
-const styleID = `linklip-follower-portal`
-function getNotionHrefStyle(containerID: string) {
-  return document.getElementById(styleID + containerID) ?? document.createElement('style')
-}
-function removeNotionHrefStyle(containerID: string) {
-  getNotionHrefStyle(containerID).innerHTML = ''
-}
-export function isRendered(el?: Element): el is HTMLElement {
-  return document.contains(el!)
 }
